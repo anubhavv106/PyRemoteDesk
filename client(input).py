9204a6b775d98c6b6f949eae28269b4b7a459437
Anubhav verma <112748712+anubhavv106@users.noreply.github.com>
2024-12-01 10:14:05 +0530

    Add files via upload

---
+++ client(input).py
@@ -0,0 +1,191 @@
+import cv2
+import numpy as np
+import pyautogui
+import socket
+import pickle
+import struct
+from threading import Thread
+from pynput.mouse import Button, Controller
+import win32gui
+import lzma
+from io import BytesIO
+from PIL import Image
+import base64
+from pynput.keyboard import Key, Listener
+import subprocess
+import mss
+import os
+import json
+from screeninfo import get_monitors
+
+# it will dislay the cursor 
+mouse = '''iVBORw0KGgoAAAANSUhEUgAAABEAAAAXCAYAAADtNKTnAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAjnRFWHRDb21tZW50AGNIUk0gY2h1bmtsZW4gMzIgaWdub3JlZA1BU0NJSTogLi56JS4uLi4uLi4lLi4uLi4ubV8uLi5sLi48Li4uLlgNSEVYOiAwMDAwN0EyNTAwMDA4MDgzMDAwMEY0MjUwMDAwODREMTAwMDA2RDVGMDAwMEU4NkMwMDAwM0M4QjAwMDAxQjU4GN773QAAAgFJREFUeJyUlEtLAlEUx0cd7YFagUkvxKIgiB4USEREUNAqWgvtatEnaNVnaVWrNrVoEURFT6JCxBDDIpKScGG5EATR2/9MZ6ZrjjJe+MHMmXt+c+65c0cRQihEuVy2lUolO0HXetwKil31valN3SFFUVygFTQDJ7CBhiSHYA45ncAHvI2INMlH+lNAcgRWkDMI+kCbVZEmwYVg0SlYRc7of5EliSS6AGtIHLMqqpBIoiuwblVUJZFE12Yis+03leBbEanUu2VRlYQExWJRFAoF8fzyWlMkL61CIgvy+bzI5XIiHk/UFdH2GxIzQTabFZlMRkQiUTPR3wdJknqCdDqN/qTE7e2dwNwrJM2AEdDFx8SuSWTB01PSEODZjcQ1uGQJVRMAHuDQJLLA1ezL7ezuaRVw4pbb27OEyfNgGkyAIdAN3IZEEnyDDcQepSXEMHEBzIJxEOSD6uGT/7scFnyBMIJziJ3s7x+KZDJJkmOwyRJaRi8LnJqAd+cIyVmwiNthMIVYGEQTiYQ4OzsnUZTk9KyiF3y69aHy3tPWDThcPX1IPCBBLBYjyT1XEuJ+dHBOxaCymvgN7WikG4kB8AC2wTLik9zUfn5hlUQXqQyValNdfjugb8HPDQ3ydavRD31N8rpMBklb+O1eFlDM+OOZ/2RqV+mQK9Dn/wAAAP//AwCGMlcrSCX+UAAAAABJRU5ErkJggg=='''
+
+
+class RemoteDesktop:
+    def __init__(self, host, port):
+        self.ip = host
+        self.port = port
+        self.active = False
+        self.socket = socket.socket()
+        self.LeftMouseup = False
+        self.RightMouseup = False
+        self.userinput = False
+        self.quality = 15
+        self.monitor = 1
+        self.powershell = False
+
+    def display(self) -> bytes:
+        screen_info = []
+        for monitor in get_monitors():
+            screen_info.append({
+                "size": (monitor.width, monitor.height),
+                "resolution": (monitor.width, monitor.height),
+                "position": (monitor.x, monitor.y),
+                "len_monitors": (len(get_monitors()) - 1)
+            })
+        json_data = json.dumps(screen_info)
+        display = json_data.encode('utf-8')
+        return display
+
+    def record(self) -> np:
+        if not self.userinput:
+            screenshot = mss.mss().grab(mss.mss().monitors[self.monitor])
+            imCursor = Image.open(BytesIO(base64.b64decode(mouse)))
+            img = Image.frombytes("RGB", screenshot.size, screenshot.bgra, "raw", "BGRX")
+            curX, curY = win32gui.GetCursorPos()
+            img.paste(imCursor, box=(curX, curY), mask=imCursor)
+            return np.array(img)
+        else:
+            screenshot = mss.mss().grab(mss.mss().monitors[self.monitor])
+            img = Image.frombytes("RGB", screenshot.size, screenshot.bgra, "raw", "BGRX")
+            return np.array(img)
+
+    def on_press(self, key):
+        keyboard = str(key)
+        # self.send(keyboard.encode('utf-8'))
+
+    def recv_msg(self):
+        return self.socket.recv(1028).decode()
+
+    def send(self, data):
+        self.socket.send(struct.pack('>L', len(data)) + data)
+
+    def shell(self, data):
+        data = "".join(data)
+        if data.startswith('cd '):
+            # Change the current working directory
+            os.chdir(data[3:])
+            self.send((os.getcwd() + ">").encode())
+        else:
+            runcmd = subprocess.Popen(data,
+                                      shell=True,
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE,
+                                      stdin=subprocess.PIPE)
+            a = bytes(os.getcwd() + ">", encoding="utf-8") + runcmd.stdout.read() + runcmd.stderr.read()
+            self.send(a)
+
+    def __servercontrol(self):
+        debug = False
+        if not debug:
+            while self.active:
+                try:
+                    data = self.recv_msg()
+                    if data.startswith("shell"):
+                        data = data.split(":", 1)
+                    else:
+                        data = data.split(":")
+                    if data[0] == "mouse":
+                        mouse = Controller()
+                        xAxis, yAxis, event, flags = int(data[1]), int(data[2]), int(data[3]), int(data[4])
+                        # print(xAxis, yAxis)
+                        if xAxis < 0 and yAxis < 0:
+                            pass
+                        else:
+                            mouse.position = (xAxis, yAxis)
+                            if flags == 1:
+                                if self.LeftMouseup:
+                                    pass
+                                else:
+                                    mouse.press(Button.left)
+                                    self.LeftMouseup = True
+                            elif flags == 0:
+                                if self.LeftMouseup:
+                                    mouse.release(Button.left)
+                                    self.LeftMouseup = False
+                                else:
+                                    pass
+                            elif event == 2:
+                                mouse.click(Button.right)
+                            elif event == 7:
+                                pyautogui.doubleClick(xAxis, yAxis)
+                            elif event == 10:
+                                if flags > 0:
+                                    mouse.scroll(0, -1)
+                                else:
+                                    mouse.scroll(0, 1)
+                    elif data[0] == "keyboard":
+                        keys = int(data[1])
+                        if keys == 13:
+                            pyautogui.press("enter")
+                        else:
+                            pyautogui.press(chr(keys))
+                    elif data[0] == "config":
+                        self.quality = int(data[1])
+                        self.monitor = int(data[3])
+                        if data[2] == "0":
+                            self.userinput = False
+                        else:
+                            self.userinput = True
+                    elif data[0] == "shell":
+                        self.shell(data[1:])
+                except:
+                    pass
+
+    def __client_streaming(self):
+        self.socket.connect((self.ip, self.port))
+        self.socket.send(self.display())
+        while self.active:
+            try:
+                frame = self.record()
+            except:
+                continue
+            _, frame = cv2.imencode('.jpeg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), self.quality])
+            data = pickle.dumps(frame, 0)
+            video = lzma.compress(data)
+            video = b"video->" + video
+            try:
+                self.send(video)
+            except ConnectionResetError:
+                self.active = False
+            except ConnectionAbortedError:
+                self.active = False
+            except BrokenPipeError:
+                self.active = False
+        cv2.destroyAllWindows()
+
+    def connect(self):
+        if self.active:
+            print("Client already up and running")
+        else:
+            self.active = True
+            client_thread = Thread(target=self.__client_streaming)
+            client_thread.start()
+            controller = Thread(target=self.__servercontrol)
+            controller.start()
+            with Listener(on_press=self.on_press) as l:
+                l.join()
+
+    def stop_stream(self):
+        if self.active:
+            self.active = False
+        else:
+            print("Client is not active!")
+
+
+if __name__ == '__main__':
+    ip = input("Enter IP address: ")
+    port = int(input("Enter port number: "))
+    remote = RemoteDesktop(ip, port)
+    remote.connect()
---
+++ server(linux).py
@@ -0,0 +1,203 @@
+import cv2
+import numpy as np
+import socket
+import pickle
+import struct
+import threading
+import lzma
+import time
+import os
+import json
+try:
+    import win32api
+    _win32 = True
+except ImportError:
+    _win32 = False
+
+SWIDTH = 960
+SHEIGHT = 720
+
+class RemoteDesktop:
+    def __init__(self, host, port):
+        self.ip = host
+        self.port = port
+        self.active = False
+        self.revshell = True
+        self.__block = threading.Lock()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.ip, self.port))
+        self.userinput = 0
+        self.monitor = 0
+        self.width = 0
+        self.height = 0
+        self.cwd = "> "
+        self.display_data = {}
+        self.position = 0    
+
+    def start_server(self):
+        if self.active:
+            print("[-] Server already running")
+        else:
+            self.active = True
+            server_thread = threading.Thread(target=self.__server_listening)
+            server_thread.start()
+
+    def __server_listening(self):
+        self.socket.listen()
+        while self.active:
+            self.__block.acquire()
+            connection, address = self.socket.accept()
+            self.__block.release()
+            t = threading.Thread(target=self.__client_connection, args=(connection, address))
+            t.start()
+            print("[+] Connection started with", address)
+    
+    def stop_server(self):
+        if self.active:
+            self.active = False
+            # Close a dummy connection to break the accept loop
+            closing_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            closing_connection.connect((self.ip, self.port))
+            closing_connection.close()
+            self.__block.acquire()
+            self.socket.close()
+            self.__block.release()
+            print("[+] Server stopped")
+        else:
+            print("[!] Server not running!")
+
+    def send(self, msg, conn):
+        conn.send(msg)
+
+    def is_between(self, num, range1, range2):
+        if range1 > range2:
+            range1, range2 = range2, range1
+        return range1 <= num <= range2
+
+    def get_pos(self):
+        for i in range(self.monitor - 1):
+            self.position += self.display_data[i]['resolution'][0]
+        return self.position
+
+    def showcords(self, event, x, y, flags, param):
+        conn = param
+        if self.is_between(x, self.width - 30, self.width - 80) and self.is_between(y, 0, 50) and event == 4 and self.userinput == 0:
+            self.userinput = 1
+        elif self.is_between(x, self.width - 30, self.width - 80) and self.is_between(y, 0, 50) and event == 4 and self.userinput == 1:
+            self.userinput = 0
+
+        if self.userinput == 1:
+            if _win32:
+                win32api.SetCursor(win32api.LoadCursor(0, 32649))
+            x = str(x + self.get_pos())
+            y = str(y + int(self.display_data[self.monitor - 1]['position'][1]))
+            event = str(event)
+            flags = str(flags)
+            data = ["mouse", x, y, event, flags]
+            keys = ":".join(data)
+            self.send(keys.encode('utf-8'), conn)
+            self.position = 0
+
+    def sortframe(self, frame_data):
+        data = lzma.decompress(frame_data)
+        frame = pickle.loads(data, fix_imports=True, encoding="bytes")
+        frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.resize(frame, (self.width, self.height))
+        return frame
+
+    def shell(self, connection):
+        while True:
+            if self.revshell:
+                try:
+                    shell = input(self.cwd)
+                    if shell == "cls" or shell == "clear":
+                        os.system("cls")
+                    elif shell == "":
+                        continue
+                    else:
+                        shell = "shell:" + shell
+                        self.send(shell.encode('utf-8'), connection)
+                        self.revshell = False
+                except EOFError:
+                    print()
+                    continue
+            else:
+                pass
+
+    def __client_connection(self, connection, address):
+        shell_thread = threading.Thread(target=self.shell, args=(connection,))
+        shell_thread.start()
+        try:
+            display = connection.recv(1028).decode()
+            self.display_data = json.loads(display)
+            self.width, self.height = int(self.display_data[0]['size'][0]), int(self.display_data[0]['size'][1])
+            self.monitor = int(self.display_data[0]['len_monitors'])
+        except Exception as e:
+            print(f"Error receiving display data: {e}")
+        
+        payload_size = struct.calcsize('>L')
+        data = b""
+        send = 0
+        cv2.namedWindow(str(address), cv2.WINDOW_KEEPRATIO)
+        cv2.resizeWindow(str(address), (SWIDTH, SHEIGHT))
+        cv2.createTrackbar("Quality", str(address), 15, 100, lambda x: x)
+        
+        if self.monitor > 0:
+            cv2.createTrackbar("Monitor", str(address), 0, self.monitor - 1, lambda y: y)
+        else:
+            print(f"[!] Invalid monitor count received: {self.monitor}")
+
+        while self.active:
+            break_loop = False
+            while len(data) < payload_size:
+                received = connection.recv(4096)
+                if received == b'':
+                    connection.close()
+                    break_loop = True
+                    break
+                data += received
+            if break_loop:
+                break
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack(">L", packed_msg_size)[0]
+            while len(data) < msg_size:
+                data += connection.recv(4096)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            if frame_data[0:7].decode() == "video->":
+                frame = self.sortframe(frame_data[7:])
+                cv2.rectangle(frame, (self.width - 30, 0), (self.width - 80, 50), (136, 8, 8), 4)
+                if self.userinput == 0:
+                    cv2.putText(frame, "Click blue box to control", (200, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                cv2.setMouseCallback(str(address), self.showcords, param=(connection))
+                cv2.imshow(str(address), frame)
+                k = cv2.waitKey(1)
+                quality = cv2.getTrackbarPos("Quality", str(address))
+                self.monitor = cv2.getTrackbarPos("Monitor", str(address)) + 1 if self.monitor > 0 else 0
+                quality = "config:" + str(quality) + ":" + str(self.userinput) + ":" + str(self.monitor)
+                if k != -1 and self.userinput == 1:
+                    keyboard = "keyboard:" + str(k)
+                    self.send(keyboard.encode('utf-8'), connection)
+                if send >= 5:
+                    self.send(str(quality).encode('utf-8'), connection)
+                    send = 0
+            else:
+                self.cwd = frame_data.decode().strip().split(">", 1)[0] + "> "
+                print(frame_data.decode().split(">", 1)[1])
+                self.revshell = True
+            send += 1
+
+if __name__ == '__main__':
+    server = RemoteDesktop('localhost', 6969)
+    server.start_server()
+    print("[+] Server Started\n[*] Listening For Incoming Connections...\n")
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\n[!] Keyboard Interrupt detected, stopping server...")
+        server.stop_server()
+        cv2.destroyAllWindows()
+        print("[+] Server Stopped")
